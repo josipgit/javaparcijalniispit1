18bb8ce1059cc1b0f91eedcbe5e66be78e9e64ca
josipgit <josipradictg@gmail.com>
2025-02-13 23:51:58 +0100

    proizvod

--- src/Proizvodnja/TestProizvod.java
+++ src/Proizvodnja/TestProizvod.java
@@ -9,7 +9,7 @@ public class TestProizvod {
     public static void main(String[] args) {
         int i = 0;
         ArrayList<Proizvod> proizvodi = new ArrayList<Proizvod>();
-        String[] productNames = {"sunka", "sir", "jogurt", "kruh", "mlijeko", "break"};
+        String[] productNames = {"sunka", "sir", "jogurt", "kruh", "mlijeko", "break", "pasteta", "riba", "kruv", "cola", "karota", "kunpiri"};
         Random random = new Random();
         while (true) {  // vrti dok ne dode break
             String randomName = productNames[random.nextInt(productNames.length)]; // Random product name
@@ -22,9 +22,18 @@ public class TestProizvod {
             if (randomName == "break") break;
         }
 
-        out.println("\n--------------------Ispis svih proizvoda u listi:---------");
+        out.println("\n--------------------Ispis svih proizvoda u Array listi:---------");
         for (Proizvod p : proizvodi) {
             out.println(p.IspisiProizvod() + " ");
         }
-    }
-}
+
+        double randomDouble = random.nextDouble();
+        float randomFloat = random.nextFloat();
+        float randomLong = random.nextLong();
+        float randomInt = random.nextInt();
+
+        out.println("\n---------------------Ispis randoma:-----------------------");
+        out.println(randomDouble + "\n" + randomFloat + "\n" + randomLong + "\n" + randomLong + "\n" + randomInt);
+
+    } // end main
+} // end class
